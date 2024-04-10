3b102aaaf7000bee7ff9f8414f8341bc81553491
Maurizio <mdottavi@gmail.com>
2024-04-10 13:41:29 +0200

    primo run di express

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const port= 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+  
+app.get('/aaa', function (req, res) {
+    res.send('Hello AAA')
+});
+
+app.listen (port,() => {console.log ("Backend partito!")});
