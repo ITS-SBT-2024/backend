c4e45b12a5fe2a9724d3ead4f57f39d1668ce7b5
filippo bucci <filippo.bucci1998@gmail.com>
2024-04-11 14:50:36 +0200

    CIAO

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-const port= 3000;
+const port = 3000;
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 });
-  
+
 app.get('/aaa', function (req, res) {
     res.send('Hello AAA')
 });
 
-app.listen (port,() => {console.log ("Backend partito!")});
+app.listen(port, () => { console.log("Hai salutato") });
