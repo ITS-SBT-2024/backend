d58098b7d2c79e3eb06cded3d70a302c51fab163
sambald03 <samuele.baldini03@gmail.com>
2024-04-11 15:58:04 +0200

    aggiornamento con richiesta con parametro utente

--- Samuele/index.js
+++ Samuele/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/SamueleBaldini', function (req, res) {
-    res.send('Ciao sono Samuele')
+app.get('/Samuele', function (req, res) {
+    const user = req.query.user;
+    if (user === undefined) {
+        res.send('Benvenuto sul nostro sito');
+    } else {
+        res.send('Bentornato Mr. ' + user);
+    }
 });
 
-app.listen(port, () => { console.log("Server avviato") });
+app.listen(port, () => { console.log("Server avviato!") });
