b322b289eea9573bf21b3f8a3f870257aeb4f50b
DiegoCardel <cardellinidiego@gmail.com>
2024-04-11 15:16:34 +0200

    Update 11/04 ore 15

--- DiegoCarde/index.js
+++ DiegoCarde/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get("/", function (req, res) {
-    const print = res.send("Home")
+    res.send("Home")
 })
 app.get("/diego", function (req, res) {
-    res.send("Pagina Diego")
+    res.send("<h1>Questo è l'h1 della page Diego</h1>")
 })
 app.listen(port, () => { console.log("Sei qui") })
