4cf1a64a58678685e7e9b2aabf981acd1c7de4fd
Axel...97 <lelialex97@gmail.com>
2024-04-11 15:16:33 +0200

    test3

--- alex_leli/test.js
+++ alex_leli/test.js
@@ -6,6 +6,4 @@ app.get('/hello', (req, res) => {
   res.send('Hello world!!! Im Alex, how are you today?');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+app.listen (port,() => {console.log ("Backend partito!")});
