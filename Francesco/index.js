c2bbaf8e5cadfb6d716395f92df8742f7a8fc7a2
franci40 <francescomarconi3@gmail.com>
2024-04-11 14:58:56 +0200

    request aggiustata

--- Francesco/index.js
+++ Francesco/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/FrancescoMarconi', function (req, res) {
-    res.send('Ciao sono gay')
+    res.send('Ciao sono Francesco')
 });
 
 app.listen(port, () => { console.log("Server avviato") });
