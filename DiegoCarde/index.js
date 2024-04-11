b98aa5f12d0cea981bff05a0be8d33df8346b42c
DiegoCardel <cardellinidiego@gmail.com>
2024-04-11 14:48:21 +0200

    Lavoro 11/04

--- DiegoCarde/index.js
+++ DiegoCarde/index.js
@@ -1,4 +1,11 @@
-console.log("Hello");
-a = 1;
+const express = require("express");
+const app = express()
+const port = 3000
 
-console.log("a=" + a);
+app.get("/", function (req, res) {
+    const print = res.send("Home")
+})
+app.get("/diego", function (req, res) {
+    res.send("Pagina Diego")
+})
+app.listen(port, () => { console.log("Sei qui") })
