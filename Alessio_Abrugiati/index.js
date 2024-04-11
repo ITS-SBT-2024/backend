cc900e25d2afca49941e1427be52b45aeb6015f6
alexis-82 <alessioabrugiati@gmail.com>
2024-04-11 16:53:48 +0200

    update

--- Alessio_Abrugiati/index.js
+++ Alessio_Abrugiati/index.js
@@ -4,6 +4,15 @@ const app = express()
 
 const port = 3000
 
+// Array di libri
+let books = [
+    { id: 1, name: 'Il nome della rosa' },
+    { id: 2, name: 'Il pendolo di Foucault' },
+    { id: 3, name: 'L\'isola del giorno prima' },
+    // Aggiungi altri libri qui...
+];
+
+
 app.get("/", (req, res) => {
     res.send("<h1>Benvenuto</h1>")
 })
@@ -16,6 +25,18 @@ app.get("/alessio", (req, res) => {
         res.send("Non riconosciuto")
     }
     res.send("Ciao")
+})
+
+// Nuova rotta per ottenere i dettagli di un libro
+app.get("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = books.find(b => b.id === id);
+
+    if (book) {
+        res.send(`ID: ${book.id}, Nome: ${book.name}`);
+    } else {
+        res.send('Libro non trovato');
+    }
 })
 
 app.listen (port,() => {console.log ("Backend partito!")});
