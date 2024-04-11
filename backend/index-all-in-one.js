5305a08fff950a83c73889a9cde1a59fac4371b3
Maurizio <mdottavi@gmail.com>
2024-04-11 16:54:13 +0200

    /books con Params in URL

--- backend/index-all-in-one.js
+++ backend/index-all-in-one.js
@@ -19,10 +19,42 @@ const userDB=[
         nome:"William Shakespeare"
     },
 ];
+const BookDB=[
+    {
+        id:"1",
+        title:"The Lord of the Rings",
+        author:"J.R.R. Tolkien"
+    },
+    {
+        id:"2",
+        title:"Uno Nessuno Centomila",
+        author:"Luigi Pirandello"
+    },
+
+];
 
 function miafunzione (a,b ) {
    b.send("Hello World from function");
 }
+app.get('/books/:id', function(req,res) {
+    const id=req.params.id;
+
+    trovato=null;
+    BookDB.forEach (u => {
+        if (u.id == id) {
+            trovato=u;
+        } 
+    });
+    if (trovato ){
+        res.statusCode=200;
+        //View
+        res.send("Trovato libro Titolo: "+trovato.title + " Autore:" + trovato.author);
+    } else {
+        res.statusCode=404;
+        //View
+        res.send("Libro non presente nella nostra libreria...");
+    }
+});
 
 
 app.post('/login', function(req,res) {
