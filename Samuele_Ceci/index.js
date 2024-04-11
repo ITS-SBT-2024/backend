6b19db1a820e53e0b9953034d969c72b68fd390e
Samuele Ceci <samuele.ceci4@gmail.com>
2024-04-11 14:50:49 +0200

    test

--- Samuele_Ceci/index.js
+++ Samuele_Ceci/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userMg= require("./model")
 
 const app = express();
 
@@ -6,9 +7,25 @@ const port = 3000;
 
 function miafunzione(a, b) {
     b.send("Hello World from function");
+
+function getLogin (req, res) {
+    const user = req.queery.user;
+    const pass = req.query.password;
+
+    let trovato = login (user, pass);
+    if (trovato) {
+        res.statusCode = 200;
+        res.send("Benvenueto "+trovato);
+    } else {
+        req.statusCode = 401;
+        res.send("Utente non valido");
+    }
+}
 }
 app.get('/', miafunzione);
 
+
+app.post("./login", get)
 app.post('/post', miafunzione);
 
 app.get('/aaa', function (req, res) {
@@ -16,3 +33,17 @@ app.get('/aaa', function (req, res) {
 });
 
 app.listen(port, () => { console.log("Backend partito!") });
+
+
+class Users {
+    static getUser (user, password){
+    let found = null;
+    userDB.forEach (u => {
+        if (u.username == user && u.password == password) {
+            found = u;
+            }
+        }); 
+        return found;
+    }
+}
+module.exports = User;
