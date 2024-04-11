405bd6859dfb1b9a4b9dfec11bd36182efe323fb
dadi985 <davidemazzanti98@gmail.com>
2024-04-11 15:39:50 +0200

    test

--- Davide/helloWorld.js
+++ Davide/helloWorld.js
@@ -3,9 +3,16 @@ const app = express();
 const port = 3000;
 
 const myFunc = (req, res) => {
-  res.send('<h1 style="color: red">Hello davide</h1>');
+  const user = req.query.user
+
+  if (user) {
+    res.send(`<h1 style="color: red">Hello ${user}!</h1>`)
+  } else {
+    res.send('<h1 style="color: red">Hello world!</h1>');
+  }
+
 }
 
-app.get('/davide', myFunc);
+app.get('/', myFunc);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
