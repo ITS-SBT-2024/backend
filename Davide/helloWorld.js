469415c525ccda06c55479ac91fe62f73675c56a
dadi985 <davidemazzanti98@gmail.com>
2024-04-11 15:21:38 +0200

    esempio express

--- Davide/helloWorld.js
+++ Davide/helloWorld.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const express = require('express')
+const app = express();
+const port = 3000;
+
+const myFunc = (req, res) => {
+  res.send('Hello davide');
+}
+
+app.get('/davide', myFunc);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
