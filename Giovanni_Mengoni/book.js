be08dff96fa6edb5fa8011e8fed80dc40b9a1885
Giovanni <giovannimengoni@gmail.com>
2024-04-12 11:44:10 +0200

    book

---
+++ Giovanni_Mengoni/book.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port= 3000;
+
+const BookDB=[
+    {
+        id:"1",
+        title:"The Lord of the Rings",
+        author:"J.R.R. Tolkien"
+    },
+    {
+        id:"2",
+        title:"Uno Nessuno Centomila",
+        author:"Luigi Pirandello"
+    },
+
+];
