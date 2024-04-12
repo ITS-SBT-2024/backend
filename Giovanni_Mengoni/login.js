a0223331805c41fc0c8c9a7082a6b9c867e29ad9
Giovanni <giovannimengoni@gmail.com>
2024-04-12 11:36:25 +0200

    111

--- Giovanni_Mengoni/login.js
+++ Giovanni_Mengoni/login.js
@@ -2,52 +2,62 @@ const express = require("express");
 const app = express();
 const port = 80;
 
-const userDB=[
+const userDB = [
     {
-        username:"paolo",
-        password:"777",
-        nome:"Paolo Rossi"
+        username: "paolo",
+        password: "777",
+        nome: "Paolo Rossi"
     },
     {
-        username:"giovanni",
-        password:"2020",
-        nome:"Giovanni Mengoni"
+        username: "giovanni",
+        password: "2020",
+        nome: "Giovanni Mengoni"
     },
     {
-        username:"maurizio",
-        password:"123",
-        nome:"Maurizio D'Ottavi"
+        username: "maurizio",
+        password: "123",
+        nome: "Maurizio D'Ottavi"
     },
     {
-        username:"gigi",
-        password:"345",
-        nome:"Pierluigi Alessandrini"
+        username: "gigi",
+        password: "345",
+        nome: "Pierluigi Alessandrini"
     },
     {
-        username:"bill",
-        password:"678",
-        nome:"William Shakespeare"
+        username: "bill",
+        password: "678",
+        nome: "William Shakespeare"
     },
 ];
 
-app.get("/login", function(req,res){
+app.get("/login", function (req, res) {
     const user = req.query.user;
     const password = req.query.password;
     trovato = null
-    userDB.forEach (u => {
-        if (u.username == user && u.password==pass) {
-            trovato=u;
-        } 
+    userDB.forEach(u => {
+        if (u.username == user && u.password == password) {
+            trovato = u;
+        }
     });
-    if (trovato ){
-        res.statusCode=200;
+    if (trovato) {
+        res.statusCode = 200;
         //View
-        res.send("Benvenuto "+trovato.nome);
+        res.send("Benvenuto " + trovato.nome);
     } else {
-        res.statusCode=401;
+        res.statusCode = 401;
         //View
         res.send("Utente non valido");
     }
 });
 
-    
+
+
+app.get('/', function (req, res) {
+    res.send('benvenuto nel Local Host!')
+});
+app.get('/aaa', function (req, res) {
+    res.send('Hello from AAA')
+});
+
+app.listen(port, () => { console.log("Backend partito!") });
+
