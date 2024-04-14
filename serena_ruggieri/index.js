9880fa8db9c29d30b3e0d5e1691a7c4d01bbe915
Serenaru <ruggieri.serena.1994@outlook.it>
2024-04-14 21:05:11 +0200

    add files

--- serena_ruggieri/index.js
+++ serena_ruggieri/index.js
@@ -1,1 +1,19 @@
 console.log("ciao ciao")
+
+//commento 
+
+const express = require("express");  //ho incluso la libreria nel programma
+
+const app = express();               //express ritorna un oggetto 
+
+const port = 3000;  
+
+//app.get('/',function (req,res))
+app.listen (port,() => {console.log ("Backend partito!))")})  
+
+
+
+//LOGIN 
+//function getlogin (req,res){
+    const user=req.query.user;
+
