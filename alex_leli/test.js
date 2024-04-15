10d23b295bc0e49e81a3afa44bce9853a43b1d9b
Axel...97 <lelialex97@gmail.com>
2024-04-15 09:43:12 +0200

    test4

--- alex_leli/test.js
+++ alex_leli/test.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
+app.get('/', function (req, res) {
   res.send('Hello world!!! Im Alex, how are you today?');
+});
+
+app.get("/user", function(req, res) {
+	let user = req.query.user;
+	if (user === "" || !user) {
+		res.send("Ciao, sconosciuto.");
+	} else {
+		res.send("Ciao, " + req.query.user);
+	}
 });
 
 app.listen (port,() => {console.log ("Backend partito!")});
