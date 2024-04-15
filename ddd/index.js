1a7925524b802e900c8c68111c561069f0381f88
Pulso21 <lucapulso@gmail.com>
2024-04-15 11:42:53 +0200

    esercizi

--- ddd/index.js
+++ ddd/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+app.use(express.json())
 // const userDB=[
 //     {
 //         username:"maurizio",
@@ -61,7 +61,12 @@ const libri = [
     },
 ];
 
-app.get('/title/:id', function (req, res) {
+//lista libri
+app.get('/libri',function(req,res){
+    res.send(libri)
+})
+//mostra libro per id
+app.get('/libri/:id', function (req, res) {
     const id = req.params.id;
     let search = null
     libri.forEach(l => {
@@ -71,25 +76,46 @@ app.get('/title/:id', function (req, res) {
     })
     if (search) {
         res.statusCode = 200;
-        res.send("Benvenuto " + search.id + " Nome " + search.titolo);
+        res.send("Benvenuto, il libro che stai cercando è " + " Nome " + search.title);
     } else {
         res.statusCode = 404;
         res.send("id non valido");
     }
 });
-
-app.post('/title ', function (req, res) {
+//aggiungi libro
+app.post('/libri ', function (req, res) {
     let newBook= {nome: req.body.nome, id: libri.length+1}
     libri.push(newBook)
     res.send({ id: newBook.id})
+    return libri
 });
 
-
-app.delete('/title ', function(req, res){
+//modificia libro
+app.put('/libri', function(req,res){
+    const id= req.params.id   
+    const index = libri.findIndex(libro => libro.id === id);
+        if (index !== -1) {
+            libri[index] = { nome: req.body.nome, id: id }
+            res.status(200).json({ message: 'Libro aggiornato correttamente.' })
+        } else {
+            res.status(404).json({ error: 'Libro non trovato con l\'ID specificato.' })
+        }
+    })
+//rimuovi un singolo libro con l'id
+app.delete('/libri/:id ', function(req, res){
     let removeBook = {id : req.body.id}
     libri.pop(removeBook)
     res.send({libri})
 })
+
+//rimuoverre tutti i libri
+app.delete('/libri', function(req, res) {
+    while (libri.length > 0) {
+        libri.pop() // Rimuove l'ultimo elemento dall'array
+    }
+    res.status(200).json({ message: 'Tutti i libri sono stati cancellati.' })
+});
+
 
 
 // app.get('/', miafunzione);
