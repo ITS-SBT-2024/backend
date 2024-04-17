7e92dea12479c2a2725b10b577b779b339c3ab9b
dadi985 <davidemazzanti98@gmail.com>
2024-04-17 12:09:56 +0200

    Implement auth check mw and login route

--- Davide/books/books.js
+++ Davide/books/books.js
@@ -14,10 +14,12 @@ app.use(nocache());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(isUserLoggedIn);
+app.use(/^\/books(.*)/, isUserLoggedIn);
 
 function isUserLoggedIn(req, res, next) {
-  if (req.cookies.auth) {
+  if (req.cookies && req.cookies.auth) {
+    console.log('logged in');
+    console.log(`url: ${req.url} - cookie: ${req.cookies.auth}`);
     next();
   } else {
     console.log('not logged in');
@@ -80,7 +82,7 @@ async function main() {
     const { title, author } = req.body;
     const id = `${booksDB.length + 1}`;
     const bookFound = booksDB.find(book => book.title === title && book.author === author);
-  
+
     if (!bookFound) {
       booksDB.push({
         id,
@@ -94,14 +96,14 @@ async function main() {
       res.status(200).send('Book already in DB');
     }
   }
-  
+
   app.get('/books/:id', getBookById);
-  
+
   function getBookById(req, res) {
     const { id } = req.params;
-  
+
     const bookFound = booksDB.find(book => id === book.id);
-  
+
     if (bookFound) {
       res.statusCode = 200;
       res.send(`${bookFound.title} by ${bookFound.author} at ID ${id}`);
@@ -110,12 +112,12 @@ async function main() {
       res.send('Book not found');
     }
   }
-  
+
   app.get('/books', getBooksDB);
-  
+
   function getBooksDB(req, res) {
     const { search } = req.query;
-  
+
     if (search) {
       console.log('query for: ' + search);
       const booksFound = booksDB.filter(book => book.title.toLowerCase().includes(search.toLowerCase()) || book.author.toLowerCase().includes(search.toLowerCase()));
@@ -124,13 +126,13 @@ async function main() {
       res.status(200).send(booksDB);
     }
   }
-  
+
   app.delete('/books/:id', deleteBookById);
-  
+
   function deleteBookById(req, res) {
     const { id } = req.params;
     const bookFound = booksDB.find(book => id === book.id);
-  
+
     if (bookFound) {
       booksDB = booksDB.filter(book => book.id !== id);
       saveBooksDB();
@@ -139,15 +141,15 @@ async function main() {
       res.send('Book not found');
     }
   }
-  
+
   app.put('/books/:id', substituteBookById);
-  
+
   function substituteBookById(req, res) {
     const { id } = req.params;
     const { title, author } = req.body;
-  
+
     const bookFound = booksDB.find(book => id === book.id);
-  
+
     if (bookFound) {
       bookFound.title = title;
       bookFound.author = author;
@@ -157,15 +159,15 @@ async function main() {
       addBook(req, res);
     }
   }
-  
+
   app.post('/books/:id', updateBookById);
-  
+
   function updateBookById(req, res) {
     const { id } = req.params;
     const { title, author } = req.body;
-  
+
     const bookFound = booksDB.find(book => id === book.id);
-  
+
     if (bookFound) {
       bookFound.title = title;
       bookFound.author = author;
@@ -175,24 +177,35 @@ async function main() {
       res.status(401).send('Book not found');
     }
   }
-  
+
   app.delete('/books', deleteBooksDB);
-  
+
   function deleteBooksDB(req, res) {
     booksDB = [];
     saveBooksDB();
     res.status(200).send('Books database has been deleted');
   }
-  
-  app.post('/login', login) 
+
+  app.post('/login', login)
 
   function login(req, res) {
     const { username, password } = req.body;
-    
+    let userFound = userDB.find(user => user.username === username && user.password === password);
 
+    if (userFound) {
+      console.log('user found');
+      res.statusCode = 200;
+      res.cookies('auth', username);
+      res.json({user: username})
+    } else {
+      console.log('user not found');
+      res.statusCode = 401;
+      res.json({msg: 'user not found'})
+    }
   }
-
-  app.listen(port, () => console.log(`Live on http://localhost:${port}/books`));
 }
+
+app.listen(port, () => console.log(`Live on http://localhost:${port}/books`));
+
 
 main();
